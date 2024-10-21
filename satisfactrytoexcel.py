d7bc78174851e51b63df36ccf2e80d70ad2a3a95
Quentin Barnes <Qbeast360@gmail.com>
2024-10-20 22:40:58 -0400

    Parsing Recipies done

--- satisfactrytoexcel.py
+++ satisfactrytoexcel.py
@@ -4,13 +4,25 @@ import re
 from util.constants import *
 from util.native_class import NativeClass
 from util.satisfactory_recipes import SatisfactoryRecipes
+from util.scraper import Scraper
 
-df = pd.read_csv("C:/Users/qbeas/Downloads/satisfactorytools-2024-10-15.csv")
-print(df.head())
+scraped = Scraper()
+result = scraped.scrape_data()
+if not result:
+    print("Failed to scrape data, check the scraper.")
+    exit()
+scraped_dataframe = pd.read_csv("scraped_data.csv")
+print(scraped_dataframe.head())
 
 sr = SatisfactoryRecipes()
+resource_df = sr.parse_data_resource()
+print(resource_df.head())
+recipe_df = sr.parse_data_recipe()
+print(recipe_df.head())
+
+# sr = SatisfactoryRecipes()
 # print(sr.find_class(NativeClass.Recipe))
 
 # Given a end product, create a data frame mapping out what the process for it would be.
 
-sr.datamineDependents("Uranium Fuel Rod")
+# sr.datamineDependents("Uranium Fuel Rod")
--- util/satisfactory_recipes.py
+++ util/satisfactory_recipes.py
@@ -1,4 +1,3 @@
-from enum import Enum
 import re
 from util.constants import json
 from util.native_class import NativeClass
@@ -9,6 +8,60 @@ class SatisfactoryRecipes:
 
     def __init__(self):
         self.data = json["data"]
+        self.resource_df = pd.DataFrame()
+        self.recipe_df = pd.DataFrame()
+
+    def parse_float_from_string(self, s):
+        match = re.search(r"[-+]?\d*\.\d+|\d+", s)
+        if match:
+            return float(match.group())
+        return None
+
+    def db_append_resource(self, data: dict) -> pd.DataFrame:
+        new_dataframe = pd.DataFrame([data])
+        if self.resource_df.empty:
+            self.resource_df = new_dataframe
+        self.resource_df = pd.concat([self.resource_df, new_dataframe], ignore_index=True)
+
+    def db_append_recipe(self, data: dict) -> pd.DataFrame:
+        new_dataframe = pd.DataFrame([data])
+        if self.recipe_df.empty:
+            self.recipe_df = new_dataframe
+        self.recipe_df = pd.concat([self.recipe_df, new_dataframe], ignore_index=True)
+
+    def parse_data_resource(self) -> pd.DataFrame:
+        """
+        Parses the data from the JSON file and returns a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the parsed data.
+        """
+        data = self.find_class(NativeClass.ResourceDescriptor)
+        for item in data:
+            self.db_append_resource(
+                {"Name": item["mDisplayName"], "Description": item["mDescription"], "Abreviation": item["mAbbreviatedDisplayName"]}
+            )
+
+        return self.resource_df
+
+    def parse_data_recipe(self) -> pd.DataFrame:
+        """
+        Parses the data from the JSON file and returns a pandas DataFrame.
+
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the parsed data.
+        """
+        data = self.find_class(NativeClass.Recipe)
+        for item in data:
+            self.db_append_recipe(
+                {
+                    "Name": item["mDisplayName"],
+                    "Ingredients": self.parse_recipe(item["mIngredients"]),
+                    "Product": self.parse_recipe(item["mProduct"]),
+                }
+            )
+
+        return self.recipe_df
 
     def find_class(self, nc: NativeClass) -> dict:
         """
@@ -24,7 +77,7 @@ class SatisfactoryRecipes:
         value = nc.value
         for item in self.data:
             if "NativeClass" in item and item["NativeClass"] == value:
-                return item
+                return item["Classes"]
 
     def parse_recipe(self, string):
         data_str = string[1:-1]
@@ -36,17 +89,57 @@ class SatisfactoryRecipes:
         matches = pattern.findall(data_str)
 
         # Convert matches to a list of dictionaries
-        return [{"ItemClass": match[0], "Amount": int(match[1])} for match in matches]
+        response = [{"ItemClass": match[0], "Amount": int(match[1])} for match in matches]
+        for item in response:
+            item_class_path = item["ItemClass"]
+            item_class_name = item_class_path.split(".")[-1].replace("'", "")
+            data = self.check_all_desc(item_class_name, "ClassName")
 
-    def find_native_classes(self, name) -> list:
+            item["Name"] = data["mDisplayName"]
+
+        return response
+
+    def find_native_classes(self, name: str, element_name: str) -> list:
         classes = []
         for item in self.data:
             if "NativeClass" in item and "Classes" in item:
                 for cls in item["Classes"]:
-                    if "mDisplayName" in cls and cls["mDisplayName"] == name:
+                    if element_name in cls and cls[element_name] == name:
                         classes.append(item["NativeClass"])
         return classes
 
-    def find_item(self, nc: NativeClass, name: str):
+    def check_all_desc(self, name: str, element_name: str):
+        classes = [
+            NativeClass.ItemDescriptor,
+            NativeClass.ItemDescriptorBiomass,
+            NativeClass.ItemDescriptorNuclearFuel,
+            NativeClass.ItemDescriptorPowerBoosterFuel,
+            NativeClass.ResourceDescriptor,
+            NativeClass.PowerShardDescriptor,
+            NativeClass.ConsumableDescriptor,
+            NativeClass.EquipmentDescriptor,
+            NativeClass.BuildingDescriptor,
+            NativeClass.PoleDescriptor,
+            NativeClass.VehicleDescriptor,
+            NativeClass.AmmoTypeInstantHit,
+            NativeClass.AmmoTypeProjectile,
+            NativeClass.AmmoTypeSpreadshot,
+        ]
+        for nc_class in classes:
+            response = self.find_item(nc_class, name, element_name)
+            if response:
+                return response
+
+        return None
+
+    def find_item(self, nc: NativeClass, name: str, element_name: str):
         class_array = self.find_class(nc)
-        matching_item = next((item for item in class_array["Classes"] if item.get("mDisplayName") == name), None)
+        matching_item = next((item for item in class_array if item.get(element_name) == name), None)
+        return matching_item
+
+
+# sr = SatisfactoryRecipes()
+# # print(sr.find_native_classes("Desc_SpikedRebar_C", "ClassName"))
+# # print(sr.parse_recipe(sr.find_item(NativeClass.Recipe, "Bauxite (Caterium)", "mDisplayName")["mIngredients"]))
+# # print(sr.parse_recipe(sr.find_item(NativeClass.Recipe, "Bauxite (Caterium)", "mDisplayName")["mProduct"]))
+# sr.parse_data()
